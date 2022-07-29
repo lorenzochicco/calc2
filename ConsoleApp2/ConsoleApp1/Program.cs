a0bc2a6ef9e2147670f1ba8881a1612025d5f5aa
lorenzochicco <110227657+lorenzochicco@users.noreply.github.com>
2022-07-29 14:55:51 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿decimal num1 = 0; decimal num2 = 0; string operator_ = ""; decimal result = 0;
+Console.WriteLine("This is a calculator in c#\r");
+Console.WriteLine("------------------------\n");
+
+Console.WriteLine("Insert a number and press enter");
+num1 = Convert.ToDecimal(Console.ReadLine());
+Console.WriteLine("Insert the numerical operator and press enter ");
+operator_ = Console.ReadLine();
+Console.WriteLine("Insert another number and press enter");
+num2 = Convert.ToDecimal(Console.ReadLine());
+
+switch (operator_)
+{
+    case "+":
+        result = num1 + num2;
+        Console.WriteLine($"Your result is: {num1} + {num2} = " + result);
+        break;
+    case "-":
+        result = num1 - num2;
+        Console.WriteLine($"Your result is: {num1} - {num2} = " + result);
+        break;
+    case "*":
+        result = num1 * num2;
+        Console.WriteLine($"Your result is: {num1} * {num2} = " + result);
+        break;
+    case "/":
+        result = num1 / num2;
+        Console.WriteLine($"Your result is: {num1} / {num2} = " + result);
+        break;
+}
+Console.WriteLine("Press any key to close the Calculator console app...");
+Console.ReadKey();
